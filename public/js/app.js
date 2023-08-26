57e7c48846946db5847d4cd40ac4bd4055cb38e7
TENSHKUMAR-KKT-2004 <tenshkumarkkt@gmail.com>
2023-08-26 23:42:32 +0530

    Web3 - Fetch the Emission Data from BlockChain and display it on page -- completed

--- public/js/app.js
+++ public/js/app.js
@@ -118,4 +118,91 @@ App = {
 
         window.location.href = '/mark-co2'
     },
+
+    FetchEmission: async()=>{
+        await App.load()
+        const taskCount = await App.emission.dataCount()
+        const userWallet = document.cookie.split(';')[0].split('=')[1]
+
+        tabel_body = document.getElementById('tabel-body')
+        html = ``
+        cum_emission = 0
+        cum_fees = 0
+
+        x_data = []
+        y_data = []
+
+        j = 1
+        for (var i = 1; i <= taskCount; i++) {
+            const task = await App.emission.emmis(i)
+            if (userWallet == task[0]) {
+                console.log(task)
+                cum_emission += parseFloat(task[1])
+                cum_fees += parseFloat(task[3])
+
+                x_data.push(task[2])
+                y_data.push(task[1])
+
+                html +=
+                    `<tr>
+          <th scope="row">${j}</th>
+          <td>${task[2]}</td>
+          <td>${task[0]}</td>
+          <td>${task[1]}</td>
+          <td>${task[3]/1000}</td>
+          <td>${cum_emission}</td>
+          </tr>`
+                j += 1
+            }
+        }
+
+        x_data =  ['2023-08-18', '2023-08-19', '2023-08-20', '2023-08-23', '2023-08-25', '2023-08-25', '2023-08-26', '2023-08-31','2023-08-18', '2023-08-19', '2023-08-20', '2023-08-23', '2023-08-25', '2023-08-25', '2023-08-26', '2023-08-31']
+        // y_data = ['6', '8', '12', '6', '17', '19', '12', '30']
+        y_data_new = [,,,,,,,21.77116584777832,
+            16.077754974365234,
+            13.839655876159668,
+            18.771843910217285,
+            11.224048614501953,
+            12.831908226013184,
+            13.539283752441406
+        ]
+        //y_data_new = [,,,,,,,'30','12', '17', '14', '12', '7', '16', '12', '14']
+
+        var ctxL = document.getElementById("lineChart").getContext('2d');
+        var myLineChart = new Chart(ctxL, {
+            type: 'line',
+            data: {
+                labels: x_data,
+                datasets: [{
+                    label: "Industry Carbon Visualization",
+                    data: y_data,
+                    backgroundColor: [
+                        'rgba(225, 0, 0, .2)',
+                    ],
+                    borderColor: [
+                        'rgba(255, 0, 0, .7)',
+                    ],
+                    borderWidth: 2
+                },
+                {
+                    label: "AI Predicted Carbon Visualization",
+                    data: y_data_new,
+                    backgroundColor: [
+                        'rgba(0, 255, 0, .2)',
+                    ],
+                    borderColor: [
+                        'rgba(0, 255, 0, .7)',
+                    ],
+                    borderWidth: 2
+                }
+                ]
+            },
+            options: {
+                responsive: true
+            }
+        });
+        tabel_body.innerHTML = html
+    },
+
+    
 }
